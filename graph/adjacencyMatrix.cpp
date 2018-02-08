6ca3920a8ba3b9c944d6c4f530038e3f91e75339
akwatz <aman.dj75@gmail.com>
2018-02-08 13:22:32 +0530

    added bfs to the implementation

--- graph/adjacencyMatrix.cpp
+++ graph/adjacencyMatrix.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
+//Defining class graph
 class graph{
 
 public:
 
-    int V;
-    vector< vector<int> > mat;
+    int V;              //number of nodes in the graph
+    vector< vector<int> > mat;          //relation between the nodes
 
+    //constructor
     graph(int v){
 
         V = v;
         
+        //allocating memory to adjacency matrix
         for(int i = 0 ; i < v ; i++){
 
             vector<int> temp(v , 0);
@@ -24,6 +26,7 @@ public:
 
     }
 
+    //functionn to add edge between nodes
     void addEdge(int src ,int dest){
 
         mat[src][dest] = 1;
@@ -31,28 +34,61 @@ public:
 
     }
 
+    //utitlity function to carry out dfs
     void dfsUtil(int parent , vector<bool> &visited){
 
-        if(visited[parent] == true)
+        if(visited[parent] == true)     //if node is visited then return
             return;
 
-        cout<<parent<<" ";
+        cout<<parent<<" ";              //visit the node
 
-        visited[parent] = true;
+        visited[parent] = true;         //mark it visited
 
+        //visit the children
         for( int i = 0 ; i < V ; i++ )
             if(mat[parent][i] == 1)
                 dfsUtil(i , visited);
 
     }
 
+    //function to call out utility function to carry dfs
     void dfs(){
 
         vector<bool> visited(V , false);
 
         dfsUtil(0 , visited);
 
-    }   
+    }  
+
+    //function to do bfs
+    void bfs(){
+
+        vector<bool> visited(V , false);        //keep track of visited nodes
+        queue<int> BfsQueue;
+
+        BfsQueue.push(0);
+
+        while(BfsQueue.empty() == false){
+
+            int parent = BfsQueue.front();      //pop the front node
+            BfsQueue.pop();
+
+            if(visited[parent] == true)         //if the node is visited then continue
+                continue;
+
+            cout<<parent<<" ";                  //visit the node
+
+            visited[parent] = true;             //mark it visited
+
+            //enqueue the children
+            for(int i = 0 ; i < V ; i++)
+                if(mat[parent][i] == 1)
+                    BfsQueue.push(i);    
+
+        }
+
+
+    } 
 
 };
 
@@ -69,6 +105,10 @@ int main(){
     demo.addEdge(2 , 6);
 
     demo.dfs();
+
+    cout<<endl;
+
+    demo.bfs();
 
     return 0;
 
